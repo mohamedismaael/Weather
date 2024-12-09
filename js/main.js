c4ecfe248c1fc345ab0dc5579589af7f3503c3dd
mohamedismaael <mohamedismaael667@gmail.com>
2024-12-09 13:36:36 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const weatherCondition = "sunny"; 
-
-  if (weatherCondition === "rain") {
-    createRainEffect();
-  } else if (weatherCondition === "sunny") {
-    createSunEffect();
-  } else if (weatherCondition === "cloudy") {
-    createCloudEffect();
-  }
-});
-
-function createRainEffect() {
-  const container = document.querySelector(".hero-section");
-  removeAllEffects(container);
-  for (let i = 0; i < 50; i++) {
-    const drop = document.createElement("div");
-    drop.classList.add("rain-drop");
-    drop.style.left = `${Math.random() * 100}%`;
-    drop.style.animationDelay = `${Math.random() * 2}s`;
-    container.appendChild(drop);
+let myInput = document.getElementById("newCountry");
+var name = document.getElementById("countryName");
+GetData();
+async function GetData() {
+  try {
+    let url = "";
+    if (myInput.value) {
+      url = `https://api.weatherapi.com/v1/current.json?key=9e74ea87dea84dc4b1b175309240712&q=${myInput.value}&aqi=yes`;
+    }
+    if (!myInput.value) {
+      url = `https://api.weatherapi.com/v1/current.json?key=9e74ea87dea84dc4b1b175309240712&q=cairo&aqi=yes`;
+    }    
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Res status: ${res.status}`);
+    }
+    const data = await res.json();
+    document.getElementById("countryName").textContent = data.location.name
+    document.getElementById("tempDegree").textContent = `${data.current.temp_c}°C`
+    console.log(data);
+  } catch (error) {
+    console.log("Error", error.message);
   }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const weatherCondition = "sunny";
+  if (weatherCondition === "sunny") {
+    createSunEffect();
+  } 
+});
 
 function createSunEffect() {
   const container = document.querySelector(".hero-section");
@@ -30,25 +39,8 @@ function createSunEffect() {
   container.appendChild(sun);
 }
 
-function createCloudEffect() {
-  const container = document.querySelector(".hero-section");
-  removeAllEffects(container);
-  for (let i = 0; i < 3; i++) {
-    const cloud = document.createElement("div");
-    cloud.classList.add("cloud");
-    cloud.style.left = `${i * 30}%`;
-    container.appendChild(cloud);
-  }
-}
-
 function removeAllEffects(container) {
-  const rainDrops = container.querySelectorAll(".rain-drop");
-  rainDrops.forEach(drop => drop.remove());
   const sun = container.querySelector(".sun");
   if (sun) sun.remove();
-  const clouds = container.querySelectorAll(".cloud");
-  clouds.forEach(cloud => cloud.remove());
+
 }
-
-
-
